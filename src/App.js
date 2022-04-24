94b12ac80ec90f2d1bb2b8c104ad671909f77d7b
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-04-23 23:00:13 -0300

    Resetando projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import Home from './components/pages/Home';
-
 import logo from './logo.svg';
 import './App.css';
 
-import NavBar from './components/layout/NavBar.js';
-import Container from './components/layout/Container';
-
 function App() {
   return (
-    <Router>
-      <NavBar />
-
-      <Container>       
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/novidades"/>
-          <Route path="/maisvistos"/>
-        </Routes>
-      </Container>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
