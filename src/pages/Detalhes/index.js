aac87090fa5a6320196cff9730bd0cf85c7e2868
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-04-25 00:23:32 -0300

    Criando a base da pagina de detalhes

--- src/pages/Detalhes/index.js
+++ src/pages/Detalhes/index.js
@@ -4,16 +4,18 @@ import {useParams} from "react-router-dom";
 import Container from "../../components/layout/Container";
 import H1 from "../../components/layout/Headings/H1";
 
+import "./Detalhes.css"
+
 const Detalhes = (props) => {
 
-    const [noticia, setNoticia ] = useState()
+    const [noticia, setNoticia ] = useState({})
 
     let {id} = useParams()
     const getNoticia = (id) => {
         let noticias = require('../../assets/noticias/noticias.json')
-        for (let noticia of noticias) {
-            if (noticia.id == id) {
-                return noticia
+        for (let item of noticias) {
+            if (item.id == id) {
+                return item
             }
         }
 
@@ -25,7 +27,18 @@ const Detalhes = (props) => {
 
     return (
         <Container>
-            <H1 texto={"Detalhes"} />
+            <div className="img-container">
+                <img className="imagem-capa" src={noticia.imagem} alt={`Capa sobre: ${noticia.titulo}`} />
+            </div>
+            <H1 texto={`${noticia.titulo}`} />
+            <div className="info-container">
+                <p>{noticia.data} <i className="bi bi-calendar"></i></p>
+                <p>{noticia.likes} <i className="bi bi-hand-thumbs-up"></i></p>
+                <p>{noticia.views} <i className="bi bi-eye"></i></p>
+            </div>
+            <div className="conteudo-container">
+                <p>{noticia.conteudo}</p>
+            </div>
         </Container>
     )
 }
