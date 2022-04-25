4f5d9ba2177a317b03fa24935a9b206b141a4a2b
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-04-25 00:55:02 -0300

    feat: Pagina dos estados

--- src/pages/Estados/index.js
+++ src/pages/Estados/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Container from "../../components/layout/Container";
 
 import H1 from '../../components/layout/Headings/H1'
 
+import './Estado.css'
+
 const Estados = () => {
+    const jsonEstados = require('../../assets/estados/estados.json')
+    const [estados, setEstados] = useState([])
+
+    useEffect(() => {
+        setEstados(jsonEstados)
+    }, [])
+
+
     return (
         <Container>
             <H1 texto="ESTADOS" />
+            <div className="container-bandeiras">
+                {estados.map((estado) => {
+                    return(
+                        <div key={estado.id} className="div-bandeira">
+                            <img className="imagem-bandeira" src={estado.bandeira} alt={`Bandeira ${estado.nome} `} />
+                            <p>{estado.nome}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </Container>
     )
 }
